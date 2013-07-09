217bec318d28a58f6d4ade9d6f3310e01f498cd6
alejito1909 <alejandroschwind@live.com>
2013-07-09 09:24:11 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
-
+var homepage = fs.readFileSync('index.html', 'utf-8');
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+  response.send(homepage);
+}); 
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
